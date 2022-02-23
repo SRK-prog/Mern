dfc9d2637b4bc8c4d7e7267d16e0d8267f3c9b02
siva <81921724+SRK-prog@users.noreply.github.com>
2022-02-23 10:50:36 +0530

    Update Nonuser.jsx

--- src/components/nonuser/Nonuser.jsx
+++ src/components/nonuser/Nonuser.jsx
@@ -5,17 +5,6 @@ import { Link } from "react-router-dom";
 function Nonuser({ posts }) {
   return (
     <div className="Noncards">
-      <div className="LoginBanner">
-        <div className="LoginBannerMsg">
-          <div className="LoginTitle">Welcome!</div>
-          <div className="LoginDesc">Register to unlock all features</div>
-          <div className="LoginBanBtn">
-            <Link className="LoginBanButton" to="signup">
-              Register
-            </Link>
-          </div>
-        </div>
-      </div>
       {posts
         .slice(0)
         .reverse()
