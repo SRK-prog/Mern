160abe7d2f8d84d6eedef3d8359587bc092e0ab3
SRK-prog <srksivaram262@gmail.com>
2022-12-28 16:12:54 +0530

    push

--- src/api/URL.js
+++ src/api/URL.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://mernstack-backend-server.herokuapp.com/api",
+  baseURL: "http://localhost:8080/api",
 });
 
 // https://mernstack-backend-server.herokuapp.com/api
